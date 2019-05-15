d566223419b11572e1df64dc603bb6c523d27500
Ken M. Haggerty <kenmhaggerty@protonmail.com>
2019-05-14 21:19:34 -0500

    Add feedback and comments

--- wheel_of_fortune.js
+++ wheel_of_fortune.js
@@ -44,7 +44,7 @@
 
 // // End of script
 
-var word = 'ENERGY';
+var word = 'ENERGY'; // [KMH] See comment (1) below
 var isGuessingLetter = false;
 var letter = 'Z';
 
@@ -52,26 +52,37 @@ console.log("Do you want to pick a letter or guess the word?");
 
 	if (isGuessingLetter) {
 
-		if (letter === word[4]) {
+		if (letter === word[4]) { // [KMH] See comment (2) below
 			console.log("_ _ _ _ " + letter + " _");
 		}
-		else if (letter === word[0]) {
+		else if (letter === word[0]) { // [KMH] See comment (2) below
 			console.log(letter + " _ _ _ _ _");
 		}
-		else if (letter === word[1]) {
+		else if (letter === word[1]) { // [KMH] See comment (2) below
 			console.log("_ " + letter + " _ " + letter + " _ " + letter);
 		}
-		else if (letter === word[2]) {
+		else if (letter === word[2]) { // [KMH] See comment (2) below
 			console.log("_ _ " + letter + " _ _ _");
 		}
 		else {
 			console.log("Sorry, no " + letter + ".");
 		}
 	}
-else {(isGuessingLetter != true)
+else {(isGuessingLetter != true) // [KMH] See comment (3) below
 
 	if (word === "SYZYGY") {
 		console.log("Congratulations! You won Wheel Of Fortune!")
 	}
 	else console.log("Nope, keep trying!");
 }
+
+// [KMH] COMMENTS
+// (1) I think this should be `SYZYGY`? (Great word by the way!!)
+// (2) This works! But since we're hardcoding our word in for now, we could probably just keep this
+//     simple — for example, `if (letter === "G")`. If we did want to make our code able to handle
+//     any value for `word`, how might we write code that loops through each letter of `word` to see
+//     if `letter` is in it?
+// (3) Just one small typo here! Since we're testing another condition, we need another `if` after
+//     the `else`, and the conditional should come before the code we want to execute (that is, the
+//     curly opening bracket `{`). So this should read `else if (isGuessingLetter != true) {`.
+
